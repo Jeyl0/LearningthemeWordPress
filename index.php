1ec4747fd41a5a3d74643a10f7f8eb3eb89971dd
Jeylo <ydervaux@msn.com>
2017-11-28 10:28:06 +0100

    Dépot initial de mon thème

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+	/**
+	*ici je suis sur le fichier par défaut pour tout thème WP.
+	*il est l'un des 2 obligatoires (l'autre étant style.css)
+	*il est utilisé pour afficher une page si aucun autre fichier n'est disponible
+	*/
+
+    get_header();
+    get_footer();
+
+?>
+
+
+        <div class="page">
+            
+        </div> <!-- /.page -->
+        
